ea9a4105f510f8aeff5c832e3efdcda03894cf55
Fabio Labella <fabio.labella2@gmail.com>
2019-04-10 19:42:28 +0100

    Experiments with FreeAp Encoding

--- modules/dynamodb/src/main/scala/dynamodb/codec/package.scala
+++ modules/dynamodb/src/main/scala/dynamodb/codec/package.scala
@@ -16,12 +16,64 @@
 
 import com.ovoenergy.comms.aws.dynamodb.model._
 
-import cats._, data._, implicits._, free._
+import cats._, data._, implicits._
 
 object SchemaEx {
-  import io.circe._
+  sealed trait FreeAp[F[_], A] {
+    final def foldMap[G[_]](f: F ~> G)(implicit G: Applicative[G]): G[A] = ???
+    //   this match {
+    //     case FreeAp.Lift(fa) => f(fa)
+    //     case FreeAp.Pure(a) => a.pure[G]
+    //     case FreeAp.Map2(fa, fb, fab) =>
+    //       (fa.foldMap(f), fb.foldMap(f)).mapN(fab)
+    //   }
 
-  type FreeAp[F[_], A] = FreeApplicative[F, A]
+    final def analyze[M: Monoid](f: F ~> λ[α => M]): M =
+      foldMap[Const[M, ?]](
+        λ[F ~> Const[M, ?]](x => Const(f(x)))
+      ).getConst
+
+    // def P = this match {
+    //   case FreeAp.Pure(a) => a.some
+    //   case _ => None
+    // }
+
+    // def L = this match {
+    //   case FreeAp.Lift(a) => a.some
+    //   case _ => None
+    // }
+
+    // def M = this match {
+    //   case FreeAp.Map2(fa, fb, f) => (fa, fb, f).some
+    //   case _ => None
+    // }
+
+  }
+  object FreeAp {
+    case class Lift[F[_], A](fa: F[A]) extends FreeAp[F, A]
+    case class One[F[_]]() extends FreeAp[F, Unit]
+    case class Zip[F[_], A, B](fa: FreeAp[F, A], fb: FreeAp[F, B])
+        extends FreeAp[F, (A, B)]
+    case class Map[F[_], A, B](fa: FreeAp[F, A], f: A => B) extends FreeAp[F, B]
+    // case class Pure[F[_], A](a: A) extends FreeAp[F, A]
+    // case class Ap[F[_], B, A](f: FreeAp[F, B => A], fa: FreeAp[F, B])
+    //     extends FreeAp[F, A]
+    // case class Map2[F[_], B, C, A](
+    //     fa: FreeAp[F, B],
+    //     fb: FreeAp[F, C],
+    //     f: (B, C) => A)
+    //     extends FreeAp[F, A]
+
+    def lift[F[_], A](fa: F[A]): FreeAp[F, A] = Lift(fa)
+
+    implicit final def freeApplicative[S[_]]: Applicative[FreeAp[S, ?]] =
+      new Applicative[FreeAp[S, ?]] {
+        def ap[A, B](f: FreeAp[S, A => B])(fa: FreeAp[S, A]): FreeAp[S, B] =
+          ???
+
+        def pure[A](a: A): FreeAp[S, A] = Map(One[S](), (_: Unit) => a)
+      }
+  }
 
   sealed trait Schema[A]
   case object Num extends Schema[Int]
@@ -34,11 +86,12 @@ object SchemaEx {
       name: String,
       elemSchema: Schema[E],
       get: R => E): FreeAp[Props[R, ?], E] =
-    FreeApplicative.lift(Props(name, elemSchema, get))
+    FreeAp.lift(Props(name, elemSchema, get))
 
   case class User(id: Int, name: String)
+  case class Role(capability: String, u: User)
 
-  val s: Schema[User] =
+  def s: Schema[User] =
     Rec(
       (
         props("id", Num, (_: User).id),
@@ -46,11 +99,49 @@ object SchemaEx {
       ).mapN(User.apply)
     )
 
-  val u = User(20, "joe")
+  def s2: Schema[Role] = Rec(
+    (
+      props("capability", Str, (_: Role).capability),
+      props("user", s, (_: Role).u)).mapN(Role.apply)
+  )
 
-  val r = Encoder.fromSchema(s)(u)
+  // def p[A](s: Schema[A]): String = s match {
+  //   case Rec(p) =>
+  //     import FreeAp._
+  //     def t[F[_], A](f: FreeAp[F, A]): String =
+  //       f match {
+  //         case Pure(a) => "pure\n"
+  //         case Lift(fa) => "lift\n"
+  //         case Map2(fa, fb, _) => t(fa) ++ "map2" ++ t(fb)
+  //       }
+  //     t(p)
+  // p match {
+  //   case FreeAp.Map2(npp, fb, f) =>
+  //     npp match {
+  //       case FreeAp.Lift(Props(a, sch, get)) =>
+  //         //FreeAp.Map2(FreeAp.Lift(Props("userId", sch, get)), fb, f)
+  //         "target"
+  //       case a =>
+  //         s"first arg not lift but $a"
+  //     }
+  //   case _ =>
+  //     "not map2"
+  // }
+  //   case _ => "not rec"
+  // }
+
+  def u = User(20, "joe")
+  def role = Role("admin", u)
+
+  def r = Encoder.fromSchema(s)(u)
 //   scala> SchemaEx.r
 // res1: com.ovoenergy.comms.aws.dynamodb.model.AttributeValue = M(Map(AttributeName(id) -> N(20), AttributeName(name) -> S(joe)))
+
+  def r2 = Encoder.fromSchema(s2)(role)
+//   scala> SchemaEx.r2
+// res2: com.ovoenergy.comms.aws.dynamodb.model.AttributeValue = M(Map(AttributeName(capability) -> S(admin), AttributeName(user) -> M(Map(AttributeName(id) -> N(20), AttributeName(name) -> S(joe)))))
+
+//  def r3 = p(s) //Encoder.fromSchema(p(s))(u)
 
   trait Encoder[A] {
     def apply(a: A): AttributeValue
@@ -65,11 +156,9 @@ object SchemaEx {
       def encodeString: String => AttributeValue = AttributeValue.s(_)
       def encodeObject[R](ap: FreeAp[Props[R, ?], R], v: R): AttributeValue.M =
         ap.analyze {
-          new (Props[R, ?] ~>(Lambda[a => AttributeValue.M])) {
-            def apply[B](p: Props[R, B]): AttributeValue.M =
-              AttributeValue.M(
-                Map(AttributeName(p.name) -> fromSchema(p.elemSchema)
-                  .apply(p.get(v))))
+          λ[Props[R, ?] ~> λ[a => AttributeValue.M]] { p =>
+            AttributeValue.M(
+              Map(AttributeName(p.name) -> fromSchema(p.elemSchema)(p.get(v))))
           }
         }
 
@@ -81,5 +170,63 @@ object SchemaEx {
 
     }
   }
-
 }
+
+// res0: SchemaEx.Schema[SchemaEx.User] = Rec(
+//   Map2(
+//     Pure(cats.SemigroupalArityFunctions$$Lambda$8221/1365146284@d7c7bd7),
+//     Map2(
+//       Map2(
+//         Pure(cats.Apply$$Lambda$8219/1793679587@50de6f7d),
+//         Lift(Props(id,Num,SchemaEx$$$Lambda$8216/567770037@73be89dc)),
+//         SchemaEx$FreeAp$$anon$2$$Lambda$8220/1257227434@7d66e64b
+//       ),
+//       Lift(Props(name,Str,SchemaEx$$$Lambda$8217/999950115@13aa88f3)),
+//       SchemaEx$FreeAp$$anon$2$$Lambda$8220/1257227434@7d66e64b
+//     ),
+//     SchemaEx$FreeAp$$anon$2$$Lambda$8220/1257227434@7d66e64b
+//   )
+// )
+
+// Rec(
+//   Map2(
+//     Pure(cats.SemigroupalArityFunctions$$Lambda$8242/1043824291@52f1d072),
+//     Map2(
+//       Map2(
+//         Pure(cats.Apply$$Lambda$8240/104302346@68c047c),
+//         Lift(Props(capability,Str,SchemaEx$$$Lambda$8243/613285585@530bfde2)),
+//         SchemaEx$FreeAp$$anon$2$$Lambda$8241/684715828@79617e36
+//       ),
+//       Lift(Props(user,Rec
+//         (Map2(
+//           Pure(cats.SemigroupalArityFunctions$$Lambda$8242/1043824291@28935feb),
+//           Map2(
+//             Map2(
+//               Pure(cats.Apply$$Lambda$8240/104302346@68c047c),
+//               Lift(Props(id,Num,SchemaEx$$$Lambda$8237/1254776692@57033e35)),
+//               SchemaEx$FreeAp$$anon$2$$Lambda$8241/684715828@79617e36
+//             ),
+//             Lift(Props(name,Str,SchemaEx$$$Lambda$8238/1148285031@4bbf0af6)),
+//             SchemaEx$FreeAp$$anon$2$$Lambda$8241/684715828@79617e36
+//         ),
+//           SchemaEx$FreeAp$$anon$2$$Lambda$8241/684715828@79617e36)
+//         ),SchemaEx$$$Lambda$8244/900307432@530f94bf)
+//       ),SchemaEx$FreeAp$$anon$2$$Lambda$8241/684715828@79617e36),
+//     SchemaEx$FreeAp$$anon$2$$Lambda$8241/684715828@79617e36)
+// )
+
+// Rec(
+//   Ap(
+//     Pure(cats.SemigroupalArityFunctions$$Lambda$9990/123158537@55b9832a),
+//     Ap(
+//       Ap(
+//         Pure(cats.Apply$$Lambda$9989/779740805@477b16c),
+//         Lift(Props(id,Num,SchemaEx$$$Lambda$9986/1225130355@1d4ed21b))
+//       ),
+//       Lift(Props(name,Str,SchemaEx$$$Lambda$9987/449885094@4bc52312))
+//     )
+//   )
+// )
+
+// pure f <*> ap(ap(pure(g), lift("id")), lift("name")))
+// res2: SchemaEx.Schema[SchemaEx.Role] = Rec(Ap(Pure(cats.SemigroupalArityFunctions$$Lambda$9990/123158537@6aa41ffa),Ap(Ap(Pure(cats.Apply$$Lambda$9989/779740805@477b16c),Lift(Props(capability,Str,SchemaEx$$$Lambda$10004/147226086@51b67605))),Lift(Props(user,Rec(Ap(Pure(cats.SemigroupalArityFunctions$$Lambda$9990/123158537@4817a59),Ap(Ap(Pure(cats.Apply$$Lambda$9989/779740805@477b16c),Lift(Props(id,Num,SchemaEx$$$Lambda$9986/1225130355@1d4ed21b))),Lift(Props(name,Str,SchemaEx$$$Lambda$9987/449885094@4bc52312))))),SchemaEx$$$Lambda$10005/1170347022@38cdec3f)))))
