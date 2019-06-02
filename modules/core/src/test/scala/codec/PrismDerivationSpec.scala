317cb3a5c950d856b180c8b96e0db2f0ee9075cb
Fabio Labella <fabio.labella2@gmail.com>
2019-06-03 00:48:55 +0100

    Add test for Prism derivation

---
+++ modules/core/src/test/scala/codec/PrismDerivationSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 OVO Energy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dynosaur
+package codec
+
+import dynosaur.macros.PrismDerive
+
+class PrismDerivationSpec extends UnitSpec {
+  sealed trait A
+  case object B extends A
+  case object C extends A
+
+  "Derived prisms" should {
+    "classify data correctly" in {
+      pendingUntilFixed {
+        info("Bare derivation seems to work, but derived Prism doesn't ")
+        val (aToOptB, btoA @ _) = PrismDerive[A, B.type]
+        val (aToOptC, cToA @ _) = PrismDerive[A, C.type]
+        val pB = Prism.derive[A, B.type]
+        val pC = Prism.derive[A, C.type]
+
+        val b: A = B
+
+        assert(aToOptB(b) === Some(B))
+        assert(aToOptC(b) === None)
+        assert(pB.tryGet(b) === Some(B))
+        // all pass except this
+        assert(pC.tryGet(b) === None)
+        ()
+      }
+    }
+  }
+}
