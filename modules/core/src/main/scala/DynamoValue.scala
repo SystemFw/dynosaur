a5f081fec7e3fcbf2e86461375dba270d3b41461
Fabio Labella <fabio.labella2@gmail.com>
2022-06-19 23:05:25 +0100

    Consistent names for attributeMap methods

--- modules/core/src/main/scala/DynamoValue.scala
+++ modules/core/src/main/scala/DynamoValue.scala
@@ -66,6 +66,12 @@ case class DynamoValue(value: AttributeValue) {
   lazy val ss: Option[NonEmptySet[String]] =
     value.hasSs.guard[Option] >> NonEmptySet.fromSet(value.ss.asScala.toSet)
 
+  /** Converts a DynamoValue to an AWS SDK-compatible attribute map, e.g. for a PutItem operation.
+    * Returns None if the value isn't backed by a map.
+    */
+  lazy val attributeMap: Option[java.util.Map[String, AttributeValue]] =
+    value.hasM.guard[Option].as(value.m)
+
   def fold[A](
       s: String => A,
       n: DynamoValue.Number => A,
@@ -221,18 +227,9 @@ object DynamoValue {
 
   /** Builds a DynamoValue from an AWS SDK-compatible attribute map, e.g. from a GetItem response.
     */
-  def fromAttributeMap(
+  def attributeMap(
       attributes: java.util.Map[String, AttributeValue]
   ): DynamoValue = make(_.m(attributes))
-
-  /** Converts a DynamoValue to an AWS SDK-compatible attribute map, e.g. for a PutItem operation.
-    * Returns None if the value isn't backed by a map.
-    */
-  def toAttributeMap(
-      value: DynamoValue
-  ): Option[java.util.Map[String, AttributeValue]] = value.m.map(_.map {
-    case (k, v) => (k, v.value)
-  }.asJava)
 
   private def make(
       build: AttributeValue.Builder => AttributeValue.Builder
